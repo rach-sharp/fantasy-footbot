208a8876d8803d77c0d5e9a5604bba4c36a45c20
Rach Sharp <rachelsharp.dev@gmail.com>
2018-08-20 20:23:59 +0100

    Setup updates

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 import setuptools
 
 setuptools.setup(
-    name='Fantasy Premier League Scout',
-    version='1.0',
+    name='fplscout',
+    version='0.1.0',
     description='Picks good teams for the UK FPL Fantasy Football game',
     long_description=open('README.md').read().strip(),
+    long_description_content_type='text/markdown',
     author='Rachel Sharp',
     author_email='rachelsharp.dev@gmail.com',
-    url='https://github.com/rachel-sharp/fantasy-scout',
+    url='https://github.com/rachel-sharp/fpl-scout',
     packages=setuptools.find_packages(),
-    install_requires=[],
+    install_requires=['clint', 'requests', 'pulp'],
     license='MIT License',
     zip_safe=False,
     keywords='football',
-    classifiers=['Packages', 'Boilerplate'],
+    classifiers=[],
     entry_points={
         "console_scripts": [
             "fpl=fantasy_scout.cli_functions:cli_main"
@@ -21,5 +22,5 @@ setuptools.setup(
     },
     data_files=[
         ("cached_api_data", [])
-    ]
+    ],
 )
